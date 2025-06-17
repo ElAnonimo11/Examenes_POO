87794d07c09d59050030c1d086327394a51abcca
ElAnonimo11 <oscarortegarios@gmail.com>
2025-06-17 14:56:59 +0200

    a

--- P1-Monticulo/monticulo.cpp
+++ P1-Monticulo/monticulo.cpp
@@ -1,5 +1,2 @@
-//
-// Created by Óscar Ortega Ríos on 17/6/25.
-//
 
 #include "monticulo.hpp"
--- P1-Monticulo/monticulo.hpp
+++ P1-Monticulo/monticulo.hpp
@@ -1,12 +1,49 @@
 #ifndef MONTICULO_HPP
 #define MONTICULO_HPP
 
+// Se quiere implementar en C++ una clase llamada “montículo” que
+// represente un árbol binario parcialmente ordenado de mínimos de tipo
+// double. Su estructura consistirá en un único atributo privado no estático
+// “m” que será un vector de la STL. A continuación, se le proporciona todas
+// las operaciones de la STL que deberá usar para manejar el montículo:
+// a. std::make_heap(): Recibe dos iteradores, principio y final, y
+// construye un montículo.
+// b. std::insert(): Recibe dos iteradores, principio y final, y un
+// elemento “x”, flotando el elemento hasta que el árbol quede  ;; Asi no funciona insert
+// reorganizado.
+// c. std::erase(): Recibe un elemento “x”, hundiendo el elemento
+// hasta que el árbol quede reorganizado.
+// Entonces, realiza, definiendo externamente a la clase, los métodos
+// siguientes:
+// a) Define la clase, incluyendo las cabeceras de los métodos
+// posteriores.
+// b) Un constructor que construya un montículo vacío.
+// c) Un constructor que construya un montículo dado un vector.
+// d) Un método insertar, que inserte un elemento en el árbol.
+// e) Un método eliminar, que elimine un elemento.
+// f) Un método que devuelva una referencia constante al vector.
+// g) Sobrecargue el operador << para que imprima los elementos del
+// montículo.
+// Reservados todos los derechos. No se permite la explotación económica ni la transformación de esta obra. Queda permitida la impresión en su totalidad.
+// a64b0469ff35958ef4ab887a898bd50bdfbbe91a-11691214
 
+#include <vector>
+#include <iostream>
 
-class Monticulo {
+class Monticulo
+{
+public:
+    Monticulo() = default;
 
+    explicit Monticulo(const std::vector<double>& v);
+    void insertar(double e);
+    void eliminar(double e);
+    const std::vector<double>& referencia_vector() const;
+
+private:
+    std::vector<double> m;
 };
 
+std::ostream& operator<<(std::ostream& os,const Monticulo& M);
 
-
-#endif //MONTICULO_HPP
+#endif
