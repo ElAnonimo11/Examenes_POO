f3ef57d684e575cb66dfa6f96194e9e84f5713e7
ElAnonimo11 <oscarortegarios@gmail.com>
2025-06-17 14:46:30 +0200

    Initial commit

---
+++ P1-Monticulo/monticulo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Óscar Ortega Ríos on 17/6/25.
+//
+
+#include "monticulo.hpp"
---
+++ P1-Monticulo/monticulo.hpp
@@ -0,0 +1,12 @@
+#ifndef MONTICULO_HPP
+#define MONTICULO_HPP
+
+
+
+class Monticulo {
+
+};
+
+
+
+#endif //MONTICULO_HPP
